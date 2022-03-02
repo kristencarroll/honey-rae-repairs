e486a88440acf451783785256df9c6b40fce76a7
Kristen Carroll <carroll.kristen@gmail.com>
2022-03-02 14:39:32 -0600

    added login and registration page

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -4,26 +4,46 @@
 //it is a CONTAINER component. It renders no HTML itself, but contains other 
 //components that are responsible for the presentation and behavior of the application
 
-import React from "react"
-//import {CustomerList} and display under h1
-import { NavBar } from "./nav/NavBar";
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 import { ApplicationViews } from "./ApplicationViews";
-import "./Repairs.css"
+import { NavBar } from "./nav/NavBar";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
+import "./Repairs.css";
 
-//(in react components, the HTML we write is called JSX)
-export const Repairs = () => {
 
-  //render CustomerList under h1
-    return (
-        <> 
-            <NavBar />
-            <h1>Honey Rae's Repair Shop</h1>
-            
-            <ApplicationViews />
-    
-        </>
-    )
-} 
+//every route has a custom rendering function that can be specified if it needs to be.
+//if you need to have some logic about which component should be rendered,
+//use custom render function to determine which component should be rendered
+//therefor if there is currently something in local storage (user has logged in),
+//then render navBar and ApplicationViews, otherwise redirect to the login 
+export const Repairs = () => (
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("honey_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          ); 
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
+);
+
 
 //NavBar is a Presentation Component. Directly expresses HTML.
 //ApplicationViews Controller Component. Its only responsibility to to control the 
